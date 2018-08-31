24ebc07aea70dc4148921cfbf7d5bb1ec4b5f19e
john- <john-@users.noreply.github.com>
2018-08-31 16:39:30 -0500

    play audio, use local libraries, move calcs to model

--- lib/TransBrowse/Controller/Main.pm
+++ lib/TransBrowse/Controller/Main.pm
@@ -1,26 +1,12 @@
 package TransBrowse::Controller::Main;
 
 use Mojo::Base 'Mojolicious::Controller';
-use Math::Round;
 use Data::Dumper;
 
 # This action will render a template
 sub xmits {
     my $self = shift;
 
-#    my $trans = $self->model->get_xmits;
-#    $trans->each( sub {
-#	if ( $_->{file} =~ /^(\d+.\d+)_(\d+).wav/ ) {
-#	    my $pos =  ($1 - int($1)) / 0.0125 ;
-#	    $pos = nearest( 0.01, $pos );
-#	    $_->{freq_detect} = $1;
-#	    $_->{pos} = $pos;
-#	    $_->{freq_rounded} = int($1) + 0.0125 * sprintf('%.f', $pos);
-#	    $_->{time} = $2;
-#	}
-#    } );
-
-#    $self->render(xmits => $trans->to_array);
     $self->render;
 }
 
@@ -28,16 +14,6 @@ sub items {
     my $self = shift;
 
     my $trans = $self->model->get_xmits;
-    $trans->each( sub {
-	if ( $_->{file} =~ /^(\d+.\d+)_(\d+).wav/ ) {
-	    my $pos =  ($1 - int($1)) / 0.0125 ;
-	    $pos = nearest( 0.01, $pos );
-	    $_->{freq_detect} = $1;
-	    $_->{pos} = $pos;
-	    $_->{freq_rounded} = int($1) + 0.0125 * sprintf('%.f', $pos);
-	    $_->{time} = $2;
-	}
-    } );
 
     $self->render( json =>  { data => $trans->to_array } );
 }
--- lib/TransBrowse/Model/Transmissions.pm
+++ lib/TransBrowse/Model/Transmissions.pm
@@ -5,6 +5,7 @@ use warnings;
 use strict;
 
 use File::Find::Rule;
+use Math::Round;
 
 use Carp ();
 use Data::Dumper;
@@ -14,18 +15,31 @@ has postgres => sub { Carp::croak 'db is required' };
 sub get_xmits {
     my $self = shift;
 
-    return 
-        $self
+    my $trans = $self
 	->postgres
 	->db
 	->query('select file, (regexp_matches(file, \'_([0-9.]+)\'))[1]::numeric as timestamp, entered from xmit_history where entered >= timestamp \'2018-08-05 14:00\' and entered <= \'2018-08-05 16:00\' order by timestamp asc limit 100')
 #	->query('select file from xmit_history limit 10 order by entered asc')
 	->hashes;
+
+    $trans->each( sub {
+        if ( $_->{file} =~ /^(\d+.\d+)_(\d+).wav/ ) {
+            my $pos =  ($1 - int($1)) / 0.0125 ;
+            $pos = nearest( 0.01, $pos );
+            $_->{freq_detect} = $1;
+            $_->{pos} = $pos;
+            $_->{freq_rounded} = int($1) + 0.0125 * sprintf('%.f', $pos);
+            $_->{time} = $2;
+        }
+    } );
+
+    return $trans;
 };
 
 sub get_full_name {
     my ($self, $file) = @_;
 
+    # right now file needs to be in main wav file directory and not a subdir
 #    say "in get_full_name: $file";
 #    my @files = File::Find::Rule->file()
 #	->name( $file )
